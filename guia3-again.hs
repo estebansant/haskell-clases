9d7f7c76d8c1b66a3f8668acf45c4afa02e72596
Esteban <estebansant30@gmail.com>
2025-11-16 20:21:00 -0300

    empezando a repasar haskell. Completado ej1 guia 3

---
+++ guia3-again.hs
@@ -0,0 +1,193 @@
+-- Ejercicio 1.
+-- a) Implementar la función parcial f :: Integer -> Integer definida por extensión de la siguiente manera:
+-- f (1) = 8
+-- f (4) = 131
+-- f (16) = 16
+-- y cuya especificación es:
+-- problema f (n : Z) : Z {
+-- requiere: {n = 1 ∨ n = 4 ∨ n = 16}
+-- asegura: {(n = 1 → res = 8) ∧ (n = 4 → res = 131) ∧ (n = 16 → res = 16)}
+-- }
+
+f :: Integer -> Integer
+f x | x==1 = 8
+    | x == 4 = 131
+    | x == 16 = 16
+
+-- b) Análogamente, especificar e implementar la función parcial g :: Integer -> Integer
+-- g(8) = 16
+-- g(16) = 4
+-- g(131) = 1
+
+g :: Integer -> Integer
+g y | y == 8 = 16
+    | y == 16 = 4
+    | y == 131 = 1
+
+-- c) A partir de las funciones definidas en los ı́tems a) y b), implementar las funciones parciales h = f ◦ g y k = g ◦ f
+
+h :: Integer -> Integer
+h x = f (g x)
+
+k :: Integer -> Integer
+k x = g (f x)
+-- Ejercicio 2. ⋆ Especificar e implementar las siguientes funciones, incluyendo su signatura.
+-- a) absoluto: calcula el valor absoluto de un número entero.
+
+
+-- b) maximoAbsoluto: devuelve el máximo entre el valor absoluto de dos números enteros.
+
+
+-- c) maximo3: devuelve el máximo entre tres números enteros.
+
+
+-- d) algunoEsCero: dados dos números racionales, decide si alguno es igual a 0 (resolverlo con y sin pattern matching).
+
+
+-- e) ambosSonCero: dados dos números racionales, decide si ambos son iguales a 0 (resolverlo con y sin pattern matching).
+
+
+-- f) enMismoIntervalo: dados dos números reales, indica si están relacionados por la relación de equivalencia en R cuyas
+-- clases de equivalencia son: (−∞, 3], (3, 7] y (7, ∞), o dicho de otra manera, si pertenecen al mismo intervalo.
+
+
+-- g) sumaDistintos: que dados tres números enteros calcule la suma sin sumar repetidos (si los hubiera).
+
+
+-- h) esMultiploDe: dados dos números naturales, decide si el primero es múltiplo del segundo.
+
+
+-- i) digitoUnidades: dado un número entero, extrae su dı́gito de las unidades.
+
+
+-- j) digitoDecenas: dado un número entero mayor a 9, extrae su dı́gito de las decenas.
+
+
+
+-- Ejercicio 3. Implementar una función estanRelacionados :: Integer -> Integer -> Bool
+-- problema estanRelacionados (a : Z, b : Z) : Bool {
+-- requiere: {a ̸= 0 ∧ b ̸= 0}
+-- asegura: {(res = true) ↔ (a ∗ a + a ∗ b ∗ k = 0 para algún k ∈ Z con k ̸= 0)}
+-- }
+-- Por ejemplo:
+-- estanRelacionados 8 2 ⇝ True
+-- estanRelacionados 7 3 ⇝ False
+-- porque existe k = −4 tal que 82 + 8 × 2 × (−4) = 0
+-- porque no existe un k entero tal que 72 + 7 × 3 × k = 0
+
+-- Ejercicio 4. ⋆ Especificar e implementar las siguientes funciones utilizando tuplas para representar pares y ternas de
+-- números.
+-- a) productoInterno: calcula el producto interno entre dos tuplas de R × R.
+
+
+-- b) esParMenor: dadas dos tuplas de R × R, decide si cada coordenada de la primera tupla es menor a la coordenada correspondiente de la segunda tupla.
+
+
+-- c) distancia: calcula la distancia euclı́dea entre dos puntos de R2 .
+
+
+-- d) sumaTerna: dada una terna de enteros, calcula la suma de sus tres elementos.
+
+
+-- e) sumarSoloMultiplos: dada una terna de números enteros y un natural, calcula la suma de los elementos de la terna que son múltiplos del número natural.
+-- Por ejemplo:
+-- sumarSoloMultiplos (10,-8,-5) 2 ⇝ 2
+-- sumarSoloMultiplos (66,21,4) 5 ⇝ 0
+-- sumarSoloMultiplos (-30,2,12) 3 ⇝ -18
+
+
+-- f) posPrimerPar: dada una terna de enteros, devuelve la posición del primer número par si es que hay alguno, o devuelve 4 si son todos impares.
+
+
+-- g) crearPar :: a -> b -> (a, b): a partir de dos componentes, crea un par con esos valores. Debe funcionar para elementos de cualquier tipo.
+
+
+-- h) invertir :: (a, b) -> (b, a): invierte los elementos del par pasado como parámetro. Debe funcionar para elementos de cualquier tipo.
+
+
+-- i) Reescribir los ejercicios productoInterno, esParMenor y distancia usando el siguiente renombre de tipos:
+-- type Punto2D = (Float, Float)
+
+
+-- Ejercicio 5. Implementar la función todosMenores :: (Integer, Integer, Integer) -> Bool
+-- problema todosMenores (t : Z × Z × Z) : Bool {
+-- requiere: {T rue}
+-- asegura: {(res = true) ↔ ((f (t0 ) > g(t0 )) ∧ (f (t1 ) > g(t1 )) ∧ (f (t2 ) > g(t2 )))}
+-- }
+
+
+-- problema f (n : Z) : Z {
+-- requiere: {T rue}
+-- asegura: {(n ≤ 7 → res = n2 ) ∧ (n > 7 → res = 2n − 1)}
+-- }
+
+
+-- problema g (n : Z) : Z {
+-- requiere: {T rue}
+-- asegura: {Si n es un número par entonces res = n/2, en caso contrario, res = 3n + 1}
+-- }
+
+
+-- Ejercicio 6. Usando los siguientes tipos:
+-- type Anio = Integer
+-- type EsBisiesto = Bool
+-- Programar la función bisiesto :: Anio -> EsBisiesto según la siguiente especificación:
+-- problema bisiesto (año : Z) : Bool {
+-- requiere: {T rue}
+-- asegura: {(res = f alse) ↔ (año no es múltiplo de 4, o bien, año es múltiplo de 100 pero no de 400)}
+-- }
+-- Por ejemplo:
+-- bisiesto 1901 ⇝ False
+-- bisiesto 1900 ⇝ False
+-- bisiesto 1904 ⇝ True
+-- bisiesto 2000 ⇝ True
+
+-- Ejercicio 7.
+-- a) Implementar la función:
+-- distanciaManhattan:: (Float, Float, Float) -> (Float, Float, Float) -> Float
+-- problema distanciaManhattan (p : R × R × R, q : R × R × R) : R {
+-- requiere: {T rue}
+-- P2
+-- asegura: {res = i=0 |pi − qi |}
+-- }
+-- Por ejemplo:
+-- distanciaManhattan (2, 3, 4) (7, 3, 8) ⇝ 9
+-- distanciaManhattan ((-1), 0, (-8.5)) (3.3, 4, (-4)) ⇝ 12.8
+
+
+-- b) Reimplementar la función teniendo en cuenta el siguiente tipo: type Punto3D = (Float, Float, Float)
+
+
+-- Ejercicio 8. Implementar la función comparar :: Integer -> Integer -> Integer
+-- problema comparar (a : Z, b : Z) : Z {
+-- requiere: {T rue}
+-- asegura: {(res = 1) ↔ (sumaUltimosDosDigitos(a) < sumaUltimosDosDigitos(b))}
+-- asegura: {(res = −1) ↔ (sumaUltimosDosDigitos(a) > sumaUltimosDosDigitos(b))}
+-- asegura: {(res = 0) ↔ (sumaUltimosDosDigitos(a) = sumaUltimosDosDigitos(b))}
+-- }
+-- problema sumaUltimosDosDigitos (x : Z) : Z {
+-- requiere: {T rue}
+-- j k
+-- asegura: {res = (|x| mód 10) + ( |x|
+-- 10
+-- mód 10)}
+-- }
+-- Por ejemplo:
+-- comparar 45 312 ⇝ -1 porque 45 ≺ 312 y 4 + 5 > 1 + 2.
+-- comparar 2312 7 ⇝ 1 porque 2312 ≺ 7 y 1 + 2 < 0 + 7.
+-- comparar 45 172 ⇝ 0 porque no vale 45 ≺ 172 ni tampoco 172 ≺ 45.
+
+
+-- Ejercicio 9. A partir de las siguientes implementaciones en Haskell, describir en lenguaje natural qué hacen y especificarlas.
+-- a) f1 :: Float -> Float
+-- f1 n | n == 0 = 1
+-- | otherwise = 0d) f4 :: Float -> Float -> Float
+-- f4 x y = ( x + y )/2
+-- b) f2 :: Float -> Float
+-- f2 n | n == 1 = 15
+-- | n == -1 = -15e) f5 :: ( Float , Float ) -> Float
+-- f5 (x , y ) = ( x + y )/2
+-- c) f3 :: Float -> Float
+-- f3 n | n <= 9 = 7
+-- | n >= 3 = 5f) f6 :: Float -> Int -> Bool
+-- f6 a b = truncate a == b
--- guia5-ejercicios.hs
+++ guia5-ejercicios.hs
@@ -12,7 +12,213 @@ sumatoria :: [Int] -> Int
 sumatoria [] = 0
 sumatoria (x:xs) = x + sumatoria xs
 
-pertenece :: Int -> [Int] -> Bool
-pertenece _ [] = False
-pertenece a (x:xs) | a==x=True
-                   | otherwise = pertenece a xs 
+-- pertenece :: Int -> [Int] -> Bool
+-- pertenece _ [] = False
+-- pertenece a (x:xs) | a==x=True
+--                    | otherwise = pertenece a xs 
+
+contarOcurrencias :: Int -> [Int] -> Int
+contarOcurrencias _ [] = 0
+contarOcurrencias n (x:xs) | n == x = 1+ contarOcurrencias n xs
+                           | otherwise = contarOcurrencias n xs 
+
+todosNumerosIguales :: Int -> [Int] -> Bool
+todosNumerosIguales _ [] = False
+todosNumerosIguales n [x] = x == n  
+todosNumerosIguales n (x:xs) = (x == n) && todosNumerosIguales n xs
+
+-- Ejercicio 1. Definir las siguientes funciones sobre listas:
+-- 1. longitud :: [t] -> Integer , que dada una lista devuelve su cantidad de elementos.
+longitudBis :: [t] -> Integer
+longitudBis [] = 0
+longitudBis (_:xs) = 1+ longitudBis xs
+
+-- 2. ultimo :: [t] -> t según la siguiente especificación:
+ultimo :: [t] -> t
+ultimo [t] = t
+ultimo (_:xs) = ultimo xs
+
+-- 3. principio :: [t] -> [t] según la siguiente especificación:
+principio :: [t] -> [t]
+principio [_] = []
+principio (x:xs) = x : principio xs
+
+-- 4. reverso :: [t] -> [t] según la siguiente especificación:
+reverso :: [t] -> [t]
+reverso lista = reversoAux lista []
+  where
+    reversoAux [] acumulador = acumulador      -- Caso base: devolver acumulador
+    reversoAux (x:xs) acumulador = reversoAux xs (x:acumulador)
+
+-- Ejercicio 2
+-- 1. pertenece :: (Eq t) => t -> [t] -> Bool
+perteneceBis :: (Eq t) => t -> [t] -> Bool
+perteneceBis _ [] = False
+perteneceBis t (x:xs) | t == x = True
+                      | otherwise = perteneceBis t xs
+
+-- 2. todosIguales :: (Eq t) => [t] -> Bool, que dada una lista devuelve verdadero sı́ y solamente sı́ todos sus ele- mentos son iguales.
+todosIguales :: (Eq t) => [t] -> Bool
+todosIguales [] = True
+todosIguales (x:xs) | sonIguales x xs = True
+                    | otherwise = False
+
+sonIguales :: (Eq t) => t -> [t] -> Bool
+sonIguales x [y] = x == y
+sonIguales x y = (x == head y ) && sonIguales x (tail y)
+
+-- 3. todosDistintos :: (Eq t) => [t] -> Bool
+todosDistintos :: (Eq t) => [t] -> Bool
+todosDistintos [] = True
+todosDistintos (x:xs) | perteneceALista x xs = False
+                      | otherwise = todosDistintos xs
+
+perteneceALista :: (Eq t) => t -> [t] -> Bool
+perteneceALista _ [] = False
+perteneceALista y (z:zs) | y == z = True
+                         | otherwise = perteneceALista y zs
+                        
+-- 4. hayRepetidos :: (Eq t) => [t] -> Bool
+hayRepetidos :: (Eq t) => [t] -> Bool
+hayRepetidos [] = False
+hayRepetidos (x:xs) | perteneceALista x xs = True
+                    | otherwise =hayRepetidos xs
+                
+-- 5. quitar :: (Eq t) => t -> [t] -> [t], que dados un entero x y una lista xs, elimina la primera aparición de x en la lista xs (de haberla).
+quitar :: (Eq t) => t -> [t] -> [t]
+quitar _ [] = []
+quitar n (x:xs) | n == x = xs
+                | otherwise = x : quitar n xs
+
+-- 6. quitarTodos :: (Eq t ) => t -> [t] -> [t], que dados un entero x y una lista xs, elimina todas las apariciones de x en la lista xs (de haberlas)
+quitarTodos :: (Eq t) => t -> [t] -> [t]
+quitarTodos _ [] = []
+quitarTodos n (x:xs) | n == x = quitarTodos n xs
+                     | otherwise = x : quitarTodos n xs
+            
+-- 7. eliminarRepetidos :: (Eq t) => [t] -> [t] que deja en la lista una única aparición de cada elemento, eliminando las repeticiones adicionales.
+eliminarRepetidos :: (Eq t) => [t] -> [t]
+eliminarRepetidos [] = []
+eliminarRepetidos (x:xs) = x : eliminarRepetidos( quitarTodos x (x:xs) )
+
+-- Ejercicio 3
+-- 2. Definir productoria
+productoria :: [Integer] -> Integer
+productoria [] = 1
+productoria (x:xs) = x * productoria xs
+
+-- 3. maximo :: [Integer] -> Integer
+maximo :: [Integer] -> Integer
+maximo [] = 0
+maximo [x] = x
+maximo (x:xs) | x >= maximo xs = x
+              | otherwise = maximo xs
+
+-- 4. sumarN :: Integer -> [Integer] -> [Integer]
+sumarN :: Integer -> [Integer] -> [Integer]
+sumarN n [] = []
+sumarN n (x:xs) = (x+n) : sumarN n xs
+
+-- 5. sumarElPrimero :: [Integer] -> [Integer]
+sumarElPrimero :: [Integer] -> [Integer]
+sumarElPrimero [] = []
+sumarElPrimero (x:xs) = sumarN x (x:xs)
+
+
+-- Ejercicio 8
+-- 1. sumaTotal :: [[Integer]] -> Integer
+sumaTotal :: [[Integer]] -> Integer
+sumaTotal [] = 0
+sumaTotal (xs:xss) = sumaElementos xs + sumaTotal xss
+    where sumaElementos [] = 0
+          sumaElementos (x:xs) = x +sumaElementos xs  
+
+-- 2. cantidadDeApariciones :: Integer -> [[Integer]] -> Integer
+cantidadDeApariciones :: Integer -> [[Integer]] -> Integer
+cantidadDeApariciones _ [] = 0
+cantidadDeApariciones n (xs:xss) = contarEnLista n xs + cantidadDeApariciones n xss
+    where contarEnLista n [] = 0
+          contarEnLista n (x:xs) | n==x=1+contarEnLista n xs
+                             | otherwise = contarEnLista n xs 
+
+
+-- 3. contarPalabras :: String ->[[String]] ->Int
+
+contarPalabras :: String ->[[String]] ->Int
+contarPalabras _ [] = 0
+contarPalabras palabra (xs:xss) = palabraEsIgual palabra xs + contarPalabras palabra xss
+    where palabraEsIgual _ [] = 0
+          palabraEsIgual elemento (x:xs) | elemento == x = 1 + palabraEsIgual elemento xs
+                                         | otherwise = palabraEsIgual elemento xs
+
+
+
+
+
+
+
+-- Ejercicio Parcial 1
+contarElemento :: String -> [String] -> Int
+contarElemento _ [] = 0
+contarElemento a (x:xs) | a == x = 1 + contarElemento a xs
+                        | otherwise = contarElemento a xs
+
+eliminarElemento :: String -> [String] -> [String]
+eliminarElemento _ [] = []
+eliminarElemento n (x:xs) | n == x = eliminarElemento n xs
+                          | otherwise = x : eliminarElemento n xs
+
+generarStock :: [String] ->[(String, Int)]
+generarStock [] = []
+generarStock (x:xs) = (x,contarElemento x (x:xs)) : generarStock (eliminarElemento x xs)
+
+-- Ejercicio Parcial 2
+
+estaEnStock :: String -> [(String, Int)] -> Bool
+estaEnStock producto [] = False
+estaEnStock producto (x:xs) | producto == fst x = True
+                            | otherwise = estaEnStock producto xs
+
+devolverCantidadDeStock :: String -> [(String, Int)] -> Int
+devolverCantidadDeStock producto [] = 0
+devolverCantidadDeStock producto (x:xs) | producto == fst x = snd x
+                                        | otherwise = devolverCantidadDeStock producto xs
+
+stockDeProducto :: [(String, Int)] -> String ->Int
+stockDeProducto (x:xs) producto | estaEnStock producto (x:xs) == False = 0
+                                | otherwise = devolverCantidadDeStock producto (x:xs)
+
+-- Ejercicio Parcial 3
+stockConPrecio :: (String, Int) ->[(String, Float)] -> Float
+stockConPrecio _ [] = 0
+stockConPrecio x (y:ys) | fst x == fst y = snd y
+                        | otherwise = stockConPrecio x ys
+
+operacionPorProducto :: (String, Int) ->[(String, Float)] -> Float
+operacionPorProducto _ [] = 0
+operacionPorProducto x (y:ys) = fromIntegral (snd x) * stockConPrecio x (y:ys)
+
+dineroEnStock :: [(String, Int)] ->[(String, Float)] -> Float
+dineroEnStock [] [] = 0
+dineroEnStock [] (y:ys) = 0
+dineroEnStock (x:xs) (y:ys) = operacionPorProducto x (y:ys) + dineroEnStock xs (y:ys)
+
+-- Ejercicio Parcial 4
+
+esMayorADiez :: [(String, Int)] -> String -> Bool
+esMayorADiez stock prod = stockDeProducto stock prod > 10
+
+aplicarDescuento :: [(String, Int)] -> (String, Float) -> (String, Float)
+aplicarDescuento stock (prod, prec)
+  | esMayorADiez stock prod = (prod, prec * 0.8)
+  | otherwise = (prod, prec)
+
+aplicarOferta :: [(String, Int)] -> [(String, Float)] -> [(String, Float)]
+aplicarOferta stock [] = []
+aplicarOferta stock (y:ys) = aplicarDescuento stock y : aplicarOferta stock ys
+
+-- aplicarOferta :: [(String, Int)] ->[(String, Float)] ->[(String,Float)]
+-- aplicarOferta _ [] = []
+-- aplicarOferta [] (y:ys) =[]
+-- aplicarOferta (x:xs) (y:ys) | 
+                            
