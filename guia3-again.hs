845650eab3e49a8fd86d7b2e73d213f8dd1ab756
Esteban <estebansant30@gmail.com>
2025-11-16 22:45:39 -0300

    completado ejercicio 5

--- guia3-again.hs
+++ guia3-again.hs
@@ -197,27 +197,33 @@ invertir :: (a,b) ->(b,a)
 invertir (a,b) = (b,a)
 
 
--- i) Reescribir los ejercicios productoInterno, esParMenor y distancia usando el siguiente renombre de tipos:
--- type Punto2D = (Float, Float)
-
-
 -- Ejercicio 5. Implementar la función todosMenores :: (Integer, Integer, Integer) -> Bool
 -- problema todosMenores (t : Z × Z × Z) : Bool {
 -- requiere: {T rue}
 -- asegura: {(res = true) ↔ ((f (t0 ) > g(t0 )) ∧ (f (t1 ) > g(t1 )) ∧ (f (t2 ) > g(t2 )))}
 -- }
-
-
 -- problema f (n : Z) : Z {
 -- requiere: {T rue}
 -- asegura: {(n ≤ 7 → res = n2 ) ∧ (n > 7 → res = 2n − 1)}
 -- }
-
-
 -- problema g (n : Z) : Z {
 -- requiere: {T rue}
 -- asegura: {Si n es un número par entonces res = n/2, en caso contrario, res = 3n + 1}
 -- }
+
+fn :: Integer -> Integer
+fn n | n <= 7 = n^2
+     | n > 7 = (2*n) -1
+
+gn :: Integer -> Integer
+gn n | mod n 2 == 0 = div n 2
+     | otherwise = (3*n) + 1
+
+todosMenores :: (Integer, Integer,Integer) -> Bool
+todosMenores (a,b,c) | (fn a > gn a) && (fn b > gn b) && (fn c > gn c) = True
+                     | otherwise = False
+
+
 
 
 -- Ejercicio 6. Usando los siguientes tipos:
