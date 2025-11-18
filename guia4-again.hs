20784abdb94bbecc1546933b6775f19c5b21af23
Esteban <estebansant30@gmail.com>
2025-11-18 17:51:47 -0300

    completado ejercicio 5

--- guia4-again.hs
+++ guia4-again.hs
@@ -54,6 +54,10 @@ sumaImpares n | n == 0 = 0
 -- medioFact 9 ; 9 ∗ 7 ∗ 5 ∗ 3 ∗ 1 ; 945.
 -- medioFact 0 ; 1.
 
+medioFact :: Integer -> Integer
+medioFact 0 = 1
+medioFact 1 = 1
+medioFact n = n*medioFact(n-2)
 
 
 
